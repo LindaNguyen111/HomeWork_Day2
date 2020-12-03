cc920532b3533c26855ba422731a0f82488681da
LindaNguyen111 <74401824+LindaNguyen111@users.noreply.github.com>
2020-12-03 21:13:21 +0700

    Update test-case6.rb

--- test-case6.rb
+++ test-case6.rb
@@ -10,7 +10,7 @@ begin
     driver.find_element(name: 'q').send_keys 'iTMS Coaching'
     driver.find_element(xpath: '//*[@id="tsf"]/div[2]/div[1]/div[3]/center/input[1]').click
     driver.manage.timeouts.implicit_wait = 3
-    iTMS = driver.find_element(xpath: '//*[@id="rso"]/div[1]/div/div[1]/a/h3/span').click
+    driver.find_element(xpath: '//*[@id="rso"]/div[1]/div/div[1]/a/h3/span').click
     driver.manage.timeouts.implicit_wait = 3
     puts driver.title
     sleep 3
